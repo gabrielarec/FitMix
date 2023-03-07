324214c61ee388576fbfd701832ebcf7daa358f1
nabeekha <nabeekha@umich.edu>
2023-03-06 21:26:02 -0500

    JS file with additional changes

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//base JavaScript files 
+//base JavaScript file
+
+//'create new workout' quiz start button
+var createWorkout = document.getElementById("")
+
+//music quiz buttons 
+//is creating an ID for each item the best option? 
+var musicButtons = document.getElementById("buttonID")
+
+//tempo quiz buttons 
+var tempoButtons = document.getElementById("tempoID")
+
+//exercise speed buttons
+var exerciseButtons = document.getElementById("exerciseButtons")
+
+//music quiz next button
+
+//clear contents of the page after each set of selections 
