7d18e0016744f3ef2d14452294df63407ae48524
nabeekha <nabeekha@umich.edu>
2023-03-09 08:09:56 -0500

    commits to local branch, non-merge

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 //base JavaScript file
+var homeWorkouts = document.getElementById("homepage-workouts")
 
 //'create new workout' quiz start button
-var createWorkout = document.getElementById("")
+var createWorkout = document.getElementById("genreSelection")
+createWorkout.addEventListener('click', function(){
+    clearScreen()
+
+
+})
+
+//write function to display the various results of each saved set of values
+function clearScreen() {
+    homeWorkouts.innerHTML = ""
+}
+
+//function to save the generated playlist and workouts into a single localstorage variable
+function saveWorkoutToLS(){
+    const code = document.getElementById("")
+}
+
+
+//write a function to select the respective genre
 
 //music quiz buttons 
 //is creating an ID for each item the best option? 
