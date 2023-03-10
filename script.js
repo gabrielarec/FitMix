49447e051f285cbae83c6956ae2d954ed12d0cb0
nabeekha <nabeekha@umich.edu>
2023-03-09 20:13:05 -0500

    js functions added for local branch, not main

--- script.js
+++ script.js
@@ -34,4 +34,43 @@ var exerciseButtons = document.getElementById("exerciseButtons")
 
 //music quiz next button
 
+//function to clear contents. Use parameters to select which elements to take in and then hide/show
+//call each time you have an old page and new page, pass in the string of ids for each element
+function changePage(elementToHide, elementToShow){
+    document.getElementById(elementToHide).classList.add("hide")
+    document.getElementById(elementToShow).classList.remove("hide")
+    
+}
+
+function startWorkout(e){
+    e.preventDefault()
+    console.log('click')
+}
+
 //clear contents of the page after each set of selections 
+startButton = document.getElementById("start-button")
+console.log(startButton)
+startButton.addEventListener('click', startWorkout)
+
+//function to save displayed workout as localStorage. Save workouts in an array and each workout as an object
+//save the results of the complete button to LS
+function savetoLS(workout){
+    var workouts = JSON.parse(localStorage.getItem("workouts")) || []
+    workouts.push(workout)
+    localStorage.setItem("workouts", JSON.stringify(workouts))
+}
+
+saveWorkoutButton = document.getElementById("save-workout")
+saveWorkoutButton.addEventListener('click', saveWorkout)
+
+//function to save workout
+function saveWorkout(){
+    //get all exercises and playlists to create workout object
+
+    //call savetoLS and pass workout object
+
+}
+
+//function to load from localstorage. Pull the array with objects, and display the one that is relevant to you
+//append new workout to the page
+
